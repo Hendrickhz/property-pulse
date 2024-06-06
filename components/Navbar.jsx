9972fa729cb6051046f89f28098e8fab57767664
Hein Htet Zaw <bogeyhz.325@gmail.com>
2024-06-06 14:06:27 +0630

    profile image added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -130,7 +130,7 @@ const Navbar = () => {
                 <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >            
+                >
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">View notifications</span>
                   <svg
@@ -168,8 +168,10 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={session?.user?.image || profileDefault}
                       alt=""
+                      width={44}
+                      height={44}
                     />
                   </button>
                 </div>
