69d94cb2b5b7fa680ec6c23f1631c82feb4f7632
Hein Htet Zaw <bogeyhz.325@gmail.com>
2024-06-06 13:56:58 +0630

    Save user data when user sign in, session data is stored

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
@@ -17,16 +19,30 @@ export const authOptions = {
     //invoked when sign in successful
     async signIn({ profile }) {
       //1. Connect the database
+      await connectDB();
       //2. Check if user exists
+      const userExist = await User.findOne({ email: profile.email });
       //3. if not, create user
+      if (!userExist) {
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       //4. return true to allow user
+      return true;
     },
 
     //modified session object
     async session({ session }) {
       //1. get user from database
+      const user = await User.findOne({ email: session.user.email });
       //2. assign user id to session
+      session.user.id = user._id;
       //3. return session
+      return session;
     },
   },
 };
