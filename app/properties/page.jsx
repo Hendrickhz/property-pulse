269e5cc0d3d03b56ce7a7ee6318bccf094a5b9e2
Hein Htet Zaw <bogeyhz.325@gmail.com>
2024-06-08 18:28:49 +0630

    search functionality added

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,22 +1,30 @@
 import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 import { fetchProperties } from "@/utils/requests";
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property.id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4 mb-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property.id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>{" "}
+    </>
   );
 };
 
